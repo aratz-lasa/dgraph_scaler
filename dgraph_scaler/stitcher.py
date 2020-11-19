81b4fa418f035a4621459d96b631fd707db81b84
aratz-lasa <aratzml@opendeusto.es>
2020-11-19 21:03:30 +0100

    Added improvements on relabelling and stitching

--- dgraph_scaler/stitcher.py
+++ dgraph_scaler/stitcher.py
@@ -8,7 +8,7 @@ from dgraph_scaler import mpi
 
 def stitch_samples(samples: List[nx.MultiDiGraph], bridges_amount: int):
     local_stitching(samples, bridges_amount)
-    distributed_stitching(samples, bridges_amount)
+    distributed_stitching_v2(samples, bridges_amount)
 
 
 def local_stitching(samples: List[nx.MultiDiGraph], bridges_amount: int):
@@ -22,6 +22,17 @@ def local_stitching(samples: List[nx.MultiDiGraph], bridges_amount: int):
         heads = random.sample(destination.nodes, k=bridges_amount)
         sample.add_edges_from(zip(tails, heads))
 
+
+def distributed_stitching_v2(samples: List[nx.MultiDiGraph], bridges_amount: int):
+    raw_samples = [list(s.nodes) for s in samples]
+    my_remote_heads = [random.sample(random.choice(raw_samples), k=bridges_amount) for _ in range(mpi.size)]
+
+    tails = [random.sample(random.choice(raw_samples), k=bridges_amount) for _ in range(mpi.size)]
+    remote_heads = mpi.comm.alltoall(my_remote_heads)
+    for i in range(mpi.size):
+        samples[0].add_edges_from(zip(remote_heads[i], tails[i]))
+
+    mpi.comm.barrier()
 
 def distributed_stitching(samples: List[nx.MultiDiGraph], bridges_amount: int):
     my_remote_heads = [random.sample(random.choice(samples).nodes, k=bridges_amount) for _ in range(mpi.size)]

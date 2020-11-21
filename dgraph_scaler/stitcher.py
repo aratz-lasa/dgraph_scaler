ebfb0a3927e45a144a25529a85e130709a70876d
aratz-lasa <aratzml@opendeusto.es>
2020-11-21 16:53:06 +0100

    Improved stitching, so now, it does not double the bridges amount between two samples
    Changed graph analysis so now you can decide to use either networkx or snappy

--- dgraph_scaler/stitcher.py
+++ dgraph_scaler/stitcher.py
@@ -8,39 +8,31 @@ from dgraph_scaler import mpi
 
 def stitch_samples(samples: List[nx.MultiDiGraph], bridges_amount: int):
     local_stitching(samples, bridges_amount)
-    distributed_stitching_v2(samples, bridges_amount)
+    distributed_stitching(samples, bridges_amount)
 
 
 def local_stitching(samples: List[nx.MultiDiGraph], bridges_amount: int):
     samples_amount = len(samples)
+    if samples_amount < 2:
+        return
     for i, sample in enumerate(samples):
         if i == samples_amount - 1:
             destination = samples[0]
         else:
             destination = samples[i + 1]
-        tails = random.sample(sample.nodes, k=bridges_amount)
-        heads = random.sample(destination.nodes, k=bridges_amount)
+        tails = random.sample(sample.nodes, k=bridges_amount//2)
+        heads = random.sample(destination.nodes, k=bridges_amount//2)
         sample.add_edges_from(zip(tails, heads))
 
 
-def distributed_stitching_v2(samples: List[nx.MultiDiGraph], bridges_amount: int):
-    raw_samples = [s.nodes for s in samples]
-    my_remote_heads = [random.sample(random.choice(raw_samples), k=bridges_amount) for _ in range(mpi.size)]
-
-    tails = [random.sample(random.choice(raw_samples), k=bridges_amount) for _ in range(mpi.size)]
-    remote_heads = mpi.comm.alltoall(my_remote_heads)
-    for i in range(mpi.size):
-        samples[0].add_edges_from(zip(remote_heads[i], tails[i]))
-
-    mpi.comm.barrier()
-
-
 def distributed_stitching(samples: List[nx.MultiDiGraph], bridges_amount: int):
-    my_remote_heads = [random.sample(random.choice(samples).nodes, k=bridges_amount) for _ in range(mpi.size)]
+    raw_samples = [list(s.nodes) for s in samples]
+    my_remote_heads = [random.sample(random.choice(raw_samples), k=bridges_amount//2) for _ in range(mpi.size)]
 
-    tails = [random.sample(random.choice(samples).nodes, k=bridges_amount) for _ in range(mpi.size)]
+    tails = [random.sample(random.choice(raw_samples), k=bridges_amount//2) for _ in range(mpi.size)]
     remote_heads = mpi.comm.alltoall(my_remote_heads)
     for i in range(mpi.size):
-        samples[0].add_edges_from(zip(remote_heads[i], tails[i]))
+        if i != mpi.rank:
+            samples[0].add_edges_from(zip(remote_heads[i], tails[i]))
 
     mpi.comm.barrier()
--- scripts/analyze_properties.py
+++ scripts/analyze_properties.py
@@ -2,12 +2,59 @@ import os
 
 import click
 import networkx as nx
+import snap
 
 
 @click.command()
 @click.argument("input_folder")
 @click.option("-e", "--extension", default=None, help="Extension of files that contain edges")
-def load_analyze_properties(input_folder, extension):
+@click.option("-nx", "--use-networkx", is_flag=True)
+def load_analyze_properties(input_folder, extension, use_networkx):
+    if use_networkx:
+        analyze_with_networkx(extension, input_folder)
+    else:
+        analyze_with_sanppy(extension, input_folder)
+
+
+def analyze_with_sanppy(extension, input_folder):
+    id_map = {}
+    next_id = 0
+    graph = snap.PNGraph.New()
+    files_read = 0
+    for file in os.listdir(input_folder):
+        if not extension or file.endswith(extension):
+            files_read += 1
+            with open(os.path.join(input_folder, file)) as file:
+                file.readline()  # Read nodes amount
+                file.readline()  # Read edges amount
+                for line in file.readlines():
+                    edge = line.rstrip().split()
+                    n1 = id_map.get(edge[0], next_id)
+                    if n1 == next_id:
+                        id_map[edge[0]] = n1
+                    next_id += 1
+                    n2 = id_map.get(edge[1], next_id)
+                    if n2 == next_id:
+                        id_map[edge[1]] = n2
+                    next_id += 1
+                    if not graph.IsNode(n1):
+                        graph.AddNode(n1)
+                    if not graph.IsNode(n2):
+                        graph.AddNode(n2)
+                    graph.AddEdge(n1, n2)
+    print(f"Nodes: {snap.CntNonZNodes(graph)}")
+    print(f"Edges: {snap.CntUniqDirEdges(graph)}")
+    print(f"Connected: {snap.IsConnected(graph)}")
+    print(f"Average degree: {snap.CntNonZNodes(graph) / snap.CntUniqDirEdges(graph)}")
+    print(f"Average clustering coefficient: {snap.GetClustCf(graph)}")
+    ef_diam_l, ef_diam_h, diam, sp = snap.GetBfsEffDiamAll(graph, 10, True)
+    print(f"Average shortest-path: {sp}")
+    print(f"Diameter: {diam}")
+    print(f"Effective diameter: {ef_diam_l} - {ef_diam_h}")
+    # TODO: a more efficient diameter: print(f"Diameter: {nx.diameter(undir_graph)}")
+
+
+def analyze_with_networkx(extension, input_folder):
     graph = nx.MultiDiGraph()
     files_read = 0
     for file in os.listdir(input_folder):
@@ -16,11 +63,10 @@ def load_analyze_properties(input_folder, extension):
             graph = nx.compose(graph, nx.read_edgelist(os.path.join(input_folder, file), create_using=nx.MultiDiGraph))
     undir_graph = nx.MultiGraph(graph)
     unmul_graph = nx.DiGraph(graph)
-
     print(f"Nodes: {graph.number_of_nodes()}")
     print(f"Edges: {graph.number_of_edges()}")
     print(f"Connected: {nx.is_connected(undir_graph)}")
-    print(f"Average degree: {graph.number_of_edges()/ graph.number_of_nodes()}")
+    print(f"Average degree: {graph.number_of_edges() / graph.number_of_nodes()}")
     print(f"Average shortest-path: {nx.average_shortest_path_length(graph)}")
     print(f"Average clustering coefficient: {nx.average_clustering(unmul_graph)}")
     print(f"SCC: {nx.number_strongly_connected_components(graph)}")
